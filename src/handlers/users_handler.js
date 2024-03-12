dacbfc3c919bc66d3e3fe6fa3139413af8ce2dc0
Mateus Bittencourt <mateus.bittencourt@outlook.com>
2024-03-12 00:11:38 -0300

    refactoring

--- src/handlers/users_handler.js
+++ src/handlers/users_handler.js
@@ -1,12 +1,12 @@
 import {
-    createUser as handler_createUser,
-    loginUser as handler_loginUser,
-    passwordRecovery as handler_passwordRecovery,
-    passwordChange as handler_passwordChange,
+    createUser,
+    loginUser,
+    passwordRecovery,
+    passwordChange,
     myEmitter
-} from "../src/users.js";
+} from "../users.js";
 
-export const createUser = async (req, res) => {
+export const createUser_handler = async (req, res) => {
     const { username, email, password } = req.body
     if (
         username == undefined ||
@@ -15,11 +15,11 @@ export const createUser = async (req, res) => {
     ){
         return res.status(400).send("Missing required fields");
     }
-    const reply = await handler_createUser(username, email, password)
+    const reply = await createUser(username, email, password)
     return res.status(reply.statusCode).send(reply.response);
 };
 
-export const loginUser = async (req, res) => {
+export const loginUser_handler = async (req, res) => {
     const { username, password } = req.body
     if (
         username == undefined ||
@@ -27,7 +27,7 @@ export const loginUser = async (req, res) => {
     ){
         return res.status(400).send("Missing required fields");
     }
-        const user = await handler_loginUser(username, password)
+        const user = await loginUser(username, password)
     if (user){
         res.header('X-Rate-Limit', 3);
         res.header('X-Rate-Limit', 3);
@@ -39,7 +39,7 @@ export const loginUser = async (req, res) => {
     return res.status(400).send("Wrong Username/email or Password");
 };
 
-export const passwordChange = async (req, res) => {
+export const passwordChange_handler = async (req, res) => {
     const { username, currentPassword, newPassword } = req.body
     if (
         username == undefined ||
@@ -48,20 +48,20 @@ export const passwordChange = async (req, res) => {
     ){
         return res.status(400).send("Missing required fields");
     }
-    return res.send(await handler_passwordChange(username, currentPassword, newPassword));
+    return res.send(await passwordChange(username, currentPassword, newPassword));
 };
 
-export const passwordRecovery = async (req, res) => {
+export const passwordRecovery_handler = async (req, res) => {
     const { email } = req.body
 
     if ( email == undefined ){
         return res.status(400).send("Missing required fields");
     }
-    handler_passwordRecovery(email)
+    passwordRecovery(email)
     return res.send();
 };
 
-export const passwordInputToken = async (req, res) => {
+export const passwordInputToken_handler = async (req, res) => {
     const { token, password } = req.body
 
     if ( token == undefined ){
@@ -73,19 +73,6 @@ export const passwordInputToken = async (req, res) => {
     });
 };
 
-export const logoutUser = async (req, res) => {
+export const logoutUser_handler = async (req, res) => {
     res.send({createUser: "This is an createUser"});
 };
-
-export const getUserByName = async (req, res) => {
-    res.send({createUser: "This is an createUser"});
-};
-
-export const updateUser = async (req, res) => {
-    res.send({createUser: "This is an createUser"});
-};
-
-export const deleteUser = async (req, res) => {
-    res.send({createUser: "This is an createUser"});
-};
-
